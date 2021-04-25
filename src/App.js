627feb634f51ffe035508a62527c9239a96d287a
cdramirez04 <minecarcox@gmail.com>
2021-04-25 15:18:55 -0500

    inicio proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Carlos Prueba
-          Modificacion Esteban
-        </p>
-       
-        <p>Sebastian prueba 2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
     </div>
   );
 }
