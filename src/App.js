0679b97092cf456ccb0e45f0a85a0aa9123e2a18
cdramirez04 <minecarcox@gmail.com>
2021-04-25 17:51:10 -0500

    Inicio proyecto

--- src/App.js
+++ src/App.js
@@ -4,27 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Modificacion Esteban
-        </p>
-       
-        <p>Sebastian prueba 2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-      <h1>Hola</h1>
->>>>>>> 627feb634f51ffe035508a62527c9239a96d287a
+      <h1>Fasilito</h1>
     </div>
   );
 }
